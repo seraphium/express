f92b70e7c7fd0f35d19898b48631a433289cc1c3
seraphium <viperking@163.com>
2015-08-21 10:15:57 +0800

    Added auth

--- app.js
+++ app.js
@@ -20,6 +20,39 @@ app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//authentication
+app.use(function(req, res, next) {
+
+  var auth;
+
+  // check whether an autorization header was send
+  if (req.headers.authorization) {
+    // only accepting basic auth, so:
+    // * cut the starting "Basic " from the header
+    // * decode the base64 encoded username:password
+    // * split the string at the colon
+    // -> should result in an array
+    auth = new Buffer(req.headers.authorization.substring(6), 'base64').toString().split(':');
+  }
+
+  // checks if:
+  // * auth array exists
+  // * first value matches the expected user
+  // * second value the expected password
+  if (!auth || auth[0] !== 'beacon' || auth[1] !== 'bk5000') {
+    // any of the tests failed
+    // send an Basic Auth request (HTTP Code: 401 Unauthorized)
+    res.statusCode = 401;
+    // MyRealmName can be changed to anything, will be prompted to the user
+    res.setHeader('WWW-Authenticate', 'Basic realm="PhotoRealm"');
+    // this will displayed in the browser when authorization is cancelled
+    res.end('Unauthorized');
+  } else {
+    // continue with processing, user was authenticated
+    next();
+  }
+});
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
